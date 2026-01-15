ea551a87bd279db52bedf13280cdbb011e3aa250
meerim0509 <massakunova@gmail.com>
2026-01-15 19:38:35 +0600

    Registration v2

--- src/kg/sim_bank/registration/Registration.java
+++ src/kg/sim_bank/registration/Registration.java
@@ -3,5 +3,6 @@ package kg.sim_bank.registration;
 public class Registration {
     public static void main(String[] args) {
         System.out.println("Registration");
+        System.out.println("Registration v2");
     }
 }
