7e4aba61dfffe48481ec9b7d59eb96f06a910e99
11th - Amorality <11th-amorality@MacBook-Air-11th.local>
2026-01-15 22:25:10 +0600

    fixed

--- src/kg/sim_bank/payment/Registration/Login.java
+++ src/kg/sim_bank/payment/Registration/Login.java
@@ -2,7 +2,7 @@ package kg.sim_bank.payment.Registration;
 
 public class Login {
     public static void main(String[] args) {
-        System.out.println("Meeri m");
-        System.out.println("Me erim0509");
+        System.out.println("Meerim one love");
+        System.out.println("Meerim0509");
     }
 }
