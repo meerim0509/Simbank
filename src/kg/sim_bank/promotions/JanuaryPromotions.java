e49030e2c38b29b852289c84784821f97667e644
meerim0509 <massakunova@gmail.com>
2026-01-15 20:46:05 +0600

    January promo 123
    January promotion1
    
    January promotion2
    
    January promotion3
    
    January promotion4
    
    January promotion5
    
    January promotion6

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,14 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+        System.out.println("January promotion");
+        System.out.println("January promotion 123");
+        System.out.println("January promotion 1234");
+        System.out.println("January promotion 1235");
+        System.out.println("January promotion 12356");
+        System.out.println("January promotion 123567");
+        System.out.println("January promotion 1235678");
+        System.out.println("January promotion 12356789");
+    }
+}
